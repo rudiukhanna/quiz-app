44eeef43af82234d1abb6dc749559500f762995e
Rudiuk Hanna <melomanna@gmail.com>
2023-05-14 12:55:25 +0300

    last changes

--- src/App.js
+++ src/App.js
@@ -10,30 +10,30 @@ const QuizContext = createContext();
 export default function App() {
   const questions = [
 		{
-			questionText: 'What is the capital of France?',
+			questionText: 'What was the Turkish city of Istanbul called before 1930?',
 			answerOptions: [
-				{ answerText: 'New York', isCorrect: false },
+				{ answerText: 'Ankara', isCorrect: false },
 				{ answerText: 'London', isCorrect: false },
-				{ answerText: 'Paris', isCorrect: true },
-				{ answerText: 'Dublin', isCorrect: false },
+				{ answerText: 'Constantinople', isCorrect: true },
+				{ answerText: 'Sofia', isCorrect: false },
 			],
 		},
 		{
-			questionText: 'Who is CEO of Tesla?',
+			questionText: 'What sport did David Beckham play?',
 			answerOptions: [
-				{ answerText: 'Jeff Bezos', isCorrect: false },
-				{ answerText: 'Elon Musk', isCorrect: true },
-				{ answerText: 'Bill Gates', isCorrect: false },
-				{ answerText: 'Tony Stark', isCorrect: false },
+				{ answerText: 'Baseball', isCorrect: false },
+				{ answerText: 'Football', isCorrect: true },
+				{ answerText: 'Basketball', isCorrect: false },
+				{ answerText: 'American Football', isCorrect: false },
 			],
 		},
 		{
-			questionText: 'The iPhone was created by which company?',
+			questionText: 'Which country in the world is believed to have the most miles of motorway?',
 			answerOptions: [
-				{ answerText: 'Apple', isCorrect: true },
-				{ answerText: 'Intel', isCorrect: false },
-				{ answerText: 'Amazon', isCorrect: false },
-				{ answerText: 'Microsoft', isCorrect: false },
+				{ answerText: 'China', isCorrect: true },
+				{ answerText: 'USA', isCorrect: false },
+				{ answerText: 'Ukraine', isCorrect: false },
+				{ answerText: 'France', isCorrect: false },
 			],
 		},
 		{
@@ -43,6 +43,60 @@ export default function App() {
 				{ answerText: '4', isCorrect: false },
 				{ answerText: '6', isCorrect: false },
 				{ answerText: '7', isCorrect: true },
+			],
+		},
+		{
+			questionText: 'In what year did The Beatles split up?',
+			answerOptions: [
+				{ answerText: '1963', isCorrect: false },
+				{ answerText: '1968', isCorrect: false },
+				{ answerText: '1972', isCorrect: false },
+				{ answerText: '1970', isCorrect: true },
+			],
+		},
+		{
+			questionText: 'Which colour pill does Neo swallow in The Matrix?',
+			answerOptions: [
+				{ answerText: 'Green', isCorrect: false },
+				{ answerText: 'Blue', isCorrect: false },
+				{ answerText: 'Orange', isCorrect: false },
+				{ answerText: 'Red', isCorrect: true },
+			],
+		},
+		{
+			questionText: 'What is the capital city of Australia?',
+			answerOptions: [
+				{ answerText: 'Perth', isCorrect: false },
+				{ answerText: 'Sydney', isCorrect: false },
+				{ answerText: 'Melbourne', isCorrect: false },
+				{ answerText: 'Canberra', isCorrect: true },
+			],
+		},
+		{
+			questionText: 'What language is spoken in Brazil?',
+			answerOptions: [
+				{ answerText: 'Spanish', isCorrect: false },
+				{ answerText: 'French', isCorrect: false },
+				{ answerText: 'Brazilian', isCorrect: false },
+				{ answerText: 'Portuguese', isCorrect: true },
+			],
+		},
+		{
+			questionText: 'How many days are there in July?',
+			answerOptions: [
+				{ answerText: '30', isCorrect: false },
+				{ answerText: '29', isCorrect: false },
+				{ answerText: '28', isCorrect: false },
+				{ answerText: '31', isCorrect: true },
+			],
+		},
+		{
+			questionText: 'Who is next in line to the British throne after King Charles III?',
+			answerOptions: [
+				{ answerText: 'Princess Anne', isCorrect: false },
+				{ answerText: 'Prince Andrew', isCorrect: false },
+				{ answerText: 'Prince Edward', isCorrect: false },
+				{ answerText: 'Prince William', isCorrect: true },
 			],
 		},
 	];
@@ -76,7 +130,9 @@ export default function App() {
 				{showScore ? (
 					<div className='score-section'>
 						You scored {score} out of {questions.length}
-            <button onClick={resetQuiz}>Play Again</button>
+						<div className='button-score'>
+						 <button onClick={resetQuiz}>Play Again</button>
+						 </div>
 					</div>
 				) : (
 					<>
